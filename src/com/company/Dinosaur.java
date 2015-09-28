c6609c073865c2ed949888a67fee44fa29dbe926
we4954cp <lalala@lsdlfsldf.com>
2015-09-27 21:32:47 -0500

    Start of dinosaur project

---
+++ src/com/company/Dinosaur.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by we4954cp on 9/27/2015.
+ */
+public class Dinosaur {
+
+    String color;
+
+    public void roar(){
+        System.out.println("GRRRRR!!!");
+    }
+
+}
---
+++ src/com/company/DinosaurSupervisor.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class DinosaurSupervisor {
+
+    public static void main(String[] args) {
+
+        Dinosaur trex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+
+        trex.roar();
+        velociraptor.roar();
+
+    }
+}
