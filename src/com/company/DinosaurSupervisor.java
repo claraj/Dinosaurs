d799719a02e01a9b91e3fe434f86a50bdbc0f1aa
we4954cp <lalala@lsdlfsldf.com>
2015-09-27 21:48:05 -0500

    Added comments

--- src/com/company/DinosaurSupervisor.java
+++ src/com/company/DinosaurSupervisor.java
@@ -4,10 +4,12 @@ public class DinosaurSupervisor {
 
     public static void main(String[] args) {
 
+        //Create dinosaur objects
         Dinosaur trex = new Dinosaur();
         Dinosaur velociraptor = new Dinosaur();
         Dinosaur diplodocus = new Dinosaur();
 
+        //Call roar methods
         trex.roar();
         velociraptor.roar();
         diplodocus.roar();
